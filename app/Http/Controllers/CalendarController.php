094212ff19f607e19cf817d104e3502c8e9f653f
mirzabushra <mirza.bushra17@gmail.com>
2023-12-04 02:27:26 +0600

    10th commit

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,11 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ClassSubjectModel;
+use App\Models\ClassTimeModel;
+use App\Models\WeekModel;
+use Auth;
 
 class CalendarController extends Controller
 {
     public function MyCalendar()
     {
+        $getRecoed=ClassSubjectModel::Myclasssubject(Auth::user()->class_id);
+        $result=array();
+        foreach($getRecoed as $value){
+            $dataS['subject_name']= $value->subject_name;
+
+            $getWeek=WeekModel::getRecord();
+            $week=array();
+            foreach($getWeek as $valueW){
+                $dataW=array();
+                $dataW['week_name']= $valueW->name;
+                $dataW['fullcalendar_day']= $valueW->fullcalendar_day;
+                $classsubject=ClassTimeModel::getRecordClassTime( $value->class_id, $value->subject_id,$valueW->id);
+                if(!empty($classsubject)){
+                    $dataW['start_time']= $classsubject->start_time;
+                    $dataW['end_time']= $classsubject->end_time;
+                    $dataW['room_number']= $classsubject->room_number;
+                    $week[]=$dataW;
+                }
+            }
+            $dataS['week']=$week;
+            $result[]=$dataS;
+        }
+        //dd($result);
+        $data['getMyTimetable'] = $result;
         $data['header_title'] = "My Calender";
         return view("student.my_calendar", $data);
     }
--- resources/views/student/my_calendar.blade.php
+++ resources/views/student/my_calendar.blade.php
@@ -29,16 +29,33 @@
 @endsection
 
 @section('script')
-<script src='{{ url('public/dist/fullcalendar/index.global.js') }}'></script>
+<script src='{{ url("public/dist/fullcalendar/index.global.js") }}'></script>
 <script type="text/javascript">
+    var events =new Array();
+    @foreach($getMyTimetable as $value)
+        @foreach($value['week'] as $week)
+            events.push({
+                title: '{{$value["subject_name"]}}',
+                daysOfWeek: [{{$week['fullcalendar_day']}}],
+                startTime: '{{$week["start_time"]}}',
+                endTime: '{{$week["end_time"]}}',
+            });
+        @endforeach
+    @endforeach
+
     var calendarID = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarID, {
         headerToolbar: {
             left:'prev,next today',
             center: 'title',
             right: 'dayGridMonth, timeGridWeek, timeGridDay, listMonth'
-        }
-    })
+        },
+        initialDate: '<?=date('Y-m-d')?>',
+        navLinks: true,
+        editable: false,
+        events: events,
+    });
+
     calendar.render();
 </script>
 @endsection
